cf99e9d55cc9d47d70b20c37482f9e31424889ba
j.sanchezgallego <j.sanchezgallego@localhost>
2014-04-18 21:17:38 +0000

    added support for hexagonal plots.

--- python/utils/bundle.py
+++ python/utils/bundle.py
@@ -18,20 +18,17 @@ import os
 from astropy.io import fits
 from astropy import wcs
 from astropy import table
+from astropy.io import ascii
 from yanny import read_yanny
 import numpy as np
 import argparse
 from matplotlib.collections import EllipseCollection
 import matplotlib.cm as cm
 from zscale import zscale
-
-
 from matplotlib import pylab as plt
-try:
-    import aplpy
-    __use_aplpy__ = True
-except:
-    __use_aplpy__ = False
+import warnings
+
+__use_aplpy__ = False
 
 
 class Bundle(object):
@@ -40,6 +37,7 @@ class Bundle(object):
 
         self.RA = float(RA)
         self.dec = float(Dec)
+        self.size = size
 
         if simbmap is None or simbmap == '':
             self.simbMapFile = os.path.join(
@@ -57,6 +55,8 @@ class Bundle(object):
         self.fibres = self.getFibreCoordinates()
         self.fibres = self.fibres[self.fibres[:, 0] <= size]
 
+        self._calculateHexagon()
+
     def getFibreCoordinates(self):
         """Returns the RA, Dec coordinates for each fibre."""
 
@@ -72,7 +72,52 @@ class Bundle(object):
 
         return fibreWorld
 
+    def _calculateHexagon(self):
+        """Calculates the vertices of the bundle hexagon."""
+
+        if self.size not in [19, 37, 61, 91, 127]:
+            self.hexagon = None
+            return
+
+        simbMapSize = self.simbMap[self.simbMap['fnumdesign'] <= self.size]
+
+        middleRow = simbMapSize[simbMapSize['decoff'] == 0]
+        topRow = simbMapSize[simbMapSize['decoff'] ==
+                             np.max(simbMapSize['decoff'])]
+        bottopRow = simbMapSize[simbMapSize['decoff'] ==
+                                np.min(simbMapSize['decoff'])]
+
+        vertice0 = middleRow[middleRow['raoff'] == np.max(middleRow['raoff'])]
+        vertice3 = middleRow[middleRow['raoff'] == np.min(middleRow['raoff'])]
+
+        vertice1 = topRow[topRow['raoff'] == np.max(topRow['raoff'])]
+        vertice2 = topRow[topRow['raoff'] == np.min(topRow['raoff'])]
+
+        vertice5 = bottopRow[bottopRow['raoff'] == np.max(bottopRow['raoff'])]
+        vertice4 = bottopRow[bottopRow['raoff'] == np.min(bottopRow['raoff'])]
+
+        hexagonOff = np.array(
+            [[vertice0['raoff'][0], vertice0['decoff'][0]],
+             [vertice1['raoff'][0], vertice1['decoff'][0]],
+             [vertice2['raoff'][0], vertice2['decoff'][0]],
+             [vertice3['raoff'][0], vertice3['decoff'][0]],
+             [vertice4['raoff'][0], vertice4['decoff'][0]],
+             [vertice5['raoff'][0], vertice5['decoff'][0]]])
+
+        raOffDeg = hexagonOff[:, 0] / 3600. / np.cos(hexagonOff[:, 1] *
+                                                     np.pi / 180.)
+        decOffDeg = hexagonOff[:, 1] / 3600.
+
+        hexagon = hexagonOff.copy()
+        hexagon[:, 0] = self.RA + raOffDeg
+        hexagon[:, 1] = self.dec + decOffDeg
+
+        self.hexagon = hexagon
+
+        return hexagon
+
     def _getImage(self, fitsFile, ext):
+
         if isinstance(fitsFile, fits.HDUList):
             image = fitsFile[ext]
             fileName = os.path.basename(fitsFile.filename())
@@ -143,6 +188,44 @@ class Bundle(object):
         fig.show_circles(self.fibres[:, 1], self.fibres[:, 2], radius,
                          **showCirclesArgs)
 
+    def plotHexagon(self, fitsFile, outputFile=None, ext=0, **kwargs):
+
+        image, fileName = self._getImage(fitsFile, ext)
+        if outputFile is None:
+            outputFile = fileName + '.pdf'
+
+        wcsImage = wcs.WCS(image.header)
+        hexagonPix = wcsImage.wcs_world2pix(self.hexagon, 0)
+
+        hexagon = np.concatenate((self.hexagon, [self.hexagon[0]]), axis=0)
+        hexagonPix = np.concatenate((hexagonPix, [hexagonPix[0]]), axis=0)
+
+        zValues = zscale(image.data)
+        imShowArgs = dict(cmap=cm.Greys_r, vmax=zValues[1], vmin=zValues[0])
+        imShowArgs.update(kwargs)
+
+        plotArgs = dict(linewidth=0.7, color='r', linestyle='solid')
+        plotArgs.update(kwargs)
+
+        if not __use_aplpy__:
+            fig, ax = plt.subplots()
+            ax.imshow(image.data, origin='lower', **imShowArgs)
+            ax.plot(hexagonPix[:, 0], hexagonPix[:, 1], **plotArgs)
+            ax.set_xlim(0.0, ax.get_xlim()[1])
+            ax.set_ylim(0.0, ax.get_ylim()[1])
+            ax.set_xlabel(r'x [pixels]')
+            ax.set_ylabel(r'y [pixels]')
+            plt.savefig(outputFile)
+            return ax
+
+        else:
+            del imShowArgs['cmap']
+            aplFig = aplpy.FITSFigure(image)
+            aplFig.show_grayscale(**imShowArgs)
+            aplFig.show_lines([hexagon.T], **plotArgs)
+            aplFig.save(outputFile)
+            return aplFig
+
     def createDS9Regions(self, outputFile=None):
 
         if outputFile is None:
@@ -160,20 +243,36 @@ global color=green font="helvetica 10 normal roman" wcs=wcs
                          '{2:.10f}) #text = {{{3}}}').format(
                 fibre[1], fibre[2], radius, int(fibre[0]))
 
+        template += '\n'
+
         out = open(outputFile, 'w')
         out.write(template)
         out.close()
+
+    def printBundle(self):
+
+        tt = table.Table(self.fibres, names=['fnumdesign', 'RA', 'Dec'],
+                         dtype=[int, float, float])
+        ascii.write(tt, format='fixed_width_two_line',
+                    formats={'RA': '{0:.12f}', 'Dec': '{0:.12f}'})
+
+    def printHexagon(self):
+
+        tt = table.Table(self.hexagon, names=['RA', 'Dec'],
+                         dtype=[float, float])
+        ascii.write(tt, format='fixed_width_two_line',
+                    formats={'RA': '{0:.12f}', 'Dec': '{0:.12f}'})
 
 
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Bundle plotting tool.')
-    parser.add_argument('file', metavar='FILE', type=fits.open,
-                        help='the FITS file to plot on.')
     parser.add_argument('RA', metavar='RA', type=float,
                         help='RA of the centre of the bundle in degrees.')
     parser.add_argument('DEC', metavar='DEC', type=float,
                         help='Dec of the centre of the bundle in degrees.')
+    parser.add_argument('--file', '-f', metavar='FILE', type=fits.open,
+                        help='the FITS file to plot on.')
     parser.add_argument('--extension', '-e', dest='ext', type=int, default=0,
                         help='the extiension of the FITS file to use')
     parser.add_argument('--simbmap', '-s', dest='simbmap', type=str,
@@ -181,9 +280,38 @@ if __name__ == '__main__':
                         'the one in $MANGACORE_DIR will be used.')
     parser.add_argument('--size', '-z', dest='size', type=int,
                         default=127, help='the size of IFU to plot.')
+    parser.add_argument('--print', '-p', dest='printFibres',
+                        help='prints the RA and Dec of the fibres to stdout.',
+                        default=False, action='store_true')
+    parser.add_argument('--hexagon', '-x', dest='hexagon',
+                        help='plots the hexagonal shape of the bundle.',
+                        default=False, action='store_true')
+    parser.add_argument('--aplpy', '-a', dest='aplpy',
+                        help='uses APLpy if available.',
+                        default=False, action='store_true')
 
     args = parser.parse_args()
 
+    if args.aplpy:
+        try:
+            import aplpy
+            __use_aplpy__ = True
+        except:
+            warnings.warn('no APLpy module found. Reverting to matplotlib.')
+            __use_aplpy__ = False
+
     bundle = Bundle(args.RA, args.DEC, simbmap=args.simbmap, size=args.size)
-    bundle.plot(args.file, ext=args.ext)
+
+    if args.file is not None:
+        if not args.hexagon:
+            bundle.plot(args.file, ext=args.ext)
+        else:
+            bundle.plotHexagon(args.file, ext=args.ext)
+
     bundle.createDS9Regions()
+
+    if args.printFibres:
+        if not args.hexagon:
+            bundle.printBundle()
+        else:
+            bundle.printHexagon()
