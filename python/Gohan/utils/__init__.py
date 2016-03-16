0245a40bccc07f9ffee1b5c7cbbb31f52d75538e
José Sánchez-Gallego <j.sanchezgallego@uky.edu>
2016-03-16 09:14:44 -0700

    fixes to the import statements

--- python/Gohan/utils/__init__.py
+++ python/Gohan/utils/__init__.py
@@ -1,12 +1,12 @@
-import pywcsgrid2 as pywcsgrid2
-from getSDSSRun import getSDSSRun
-from sortTargets import sortTargets
-from assignIFUDesigns import assignIFUDesigns
-from utils import *
-from selectSkies import selectSkies
+import Gohan.utils.pywcsgrid2 as pywcsgrid2
+from Gohan.utils.getSDSSRun import getSDSSRun
+from Gohan.utils.sortTargets import sortTargets
+from Gohan.utils.assignIFUDesigns import assignIFUDesigns
+from Gohan.utils.utils import *
+from Gohan.utils.selectSkies import selectSkies
 
 try:
-    from Staralt import Staralt
+    from Gohan.utils.Staralt import Staralt
 except:
     # print '{0}[WARNING]: {1}no Staralt functionality available.'.format(
     #     colorama.Fore.YELLOW, colorama.Style.RESET_ALL)
