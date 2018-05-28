5807b4d0108a4c2f8ebae845138a4a0f91b91cd8
José Sánchez-Gallego <gallegoj@uw.edu>
2018-05-27 20:33:21 -0700

    Cleaned up __init__

--- python/Gohan/__init__.py
+++ python/Gohan/__init__.py
@@ -1,17 +1,28 @@
-from astropy.wcs import FITSFixedWarning
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2018-05-26
+# @Filename: __init__.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+# @Copyright: José Sánchez-Gallego
 
 import os
 import warnings
 
+from astropy.wcs import FITSFixedWarning
+
+from Gohan.core.configuration import get_config
+from Gohan.core.logger import log
 from Gohan.exceptions import GohanWarning
 
 
 def readPath(path):
-    """Get a Totoro-formatted path and returns the real path.
+    """Expands a path.
 
     Paths are expanded depending on the first characher of the input string.
     If the first character is '+', the path is considered to be
-    Totoro-internal relative to the root of the package. Otherwise, the path
+    Gohan-internal relative to the root of the package. Otherwise, the path
     is expanded using os.path.expandvars and os.path.expanduser. So,
     environment variables and user tildes '~' are valid in any path.
 
@@ -37,19 +48,8 @@ warnings.filterwarnings('always', category=GohanWarning)
 
 
 # Reads the configuration file
-from Gohan.core.configuration import get_config
 config = get_config('~/.gohan/gohan.yaml')
 
 # Creates the custom logging system
-from Gohan.core.logger import initLog
-log = initLog()
-
-# from Gohan.PlateInput import PlateInput
-# from Gohan.PlateMags import PlateMags
-# from Gohan.PlateDefinition import PlateDefinition
-# from Gohan.PlateTargets import PlateTargets
-# from Gohan.StarPlateTargets import StarPlateTargets
-# from Gohan.StandardPlateTargets import StandardPlateTargets
-#
-# __all__ = ['PlateInput', 'PlateMags', 'PlateTargets',
-#            'log', 'config', 'PlateDefinition']
+log.start_file_logger('.gohan/gohan')
+log.sh.setLevel(10)
