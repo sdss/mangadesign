00b2c8d73f2ad4fb148f2c5dabc2fa75aed785f2
José Sánchez-Gallego <gallegoj@uw.edu>
2018-12-11 13:07:05 -0800

    Update logger file

--- python/Gohan/core/logger.py
+++ python/Gohan/core/logger.py
@@ -1,48 +1,48 @@
-#!/usr/bin/env python
 # encoding: utf-8
 #
-# logger.py
-#
-# Created by José Sánchez-Gallego on 17 Sep 2017.
+# @Author: José Sánchez-Gallego
+# @Filename: logger.py
+# @License: BSD 3-Clause
+# @Copyright: José Sánchez-Gallego
 
+# Adapted from astropy's logging system.
 
-from __future__ import absolute_import, division, print_function
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import collections
 import datetime
 import logging
 import os
-import pathlib
 import re
 import shutil
 import sys
 import traceback
 import warnings
-from logging import PercentStyle
 from logging.handlers import TimedRotatingFileHandler
 
-import click
 from pygments import highlight
-from pygments.formatters import TerminalFormatter  # pylint:disable-msg=E0611
+from pygments.formatters import TerminalFormatter
 from pygments.lexers import get_lexer_by_name
 
+from .colourPrint import _color_text
 
-# Adds custom log level for important messages
+
+# Adds custom log level for print and twisted messages
 IMPORTANT = 25
 logging.addLevelName(IMPORTANT, 'IMPORTANT')
 
 
-def important(self, message, *args, **kws):
+def important_log_level(self, message, *args, **kws):
     self._log(IMPORTANT, message, args, **kws)
 
 
-logging.Logger.important = important
+logging.Logger._important = important_log_level
 
 
-def print_exception_formatted(typ, value, tb):
+def print_exception_formatted(type, value, tb):
     """A custom hook for printing tracebacks with colours."""
 
-    tbtext = ''.join(traceback.format_exception(typ, value, tb))
+    tbtext = ''.join(traceback.format_exception(type, value, tb))
     lexer = get_lexer_by_name('pytb', stripall=True)
     formatter = TerminalFormatter()
     sys.stderr.write(highlight(tbtext, lexer, formatter))
@@ -55,8 +55,7 @@ def colored_formatter(record):
                'debug': ('magenta', 'normal'),
                'warning': ('yellow', 'normal'),
                'print': ('green', 'normal'),
-               'error': ('red', 'bold'),
-               'important': ('green', 'bold')}
+               'error': ('red', 'bold')}
 
     levelname = record.levelname.lower()
 
@@ -65,18 +64,18 @@ def colored_formatter(record):
 
     if levelname.lower() in colours:
         levelname_color = colours[levelname][0]
-        bold = True if colours[levelname][1] == 'bold' else False
-        header = click.style('[{}]: '.format(levelname.upper()), levelname_color, bold=bold)
+        header = _color_text('[{}]: '.format(levelname.upper()),
+                             levelname_color)
 
     message = record.getMessage()
 
     if levelname == 'warning':
-        warning_category_groups = re.match(r'^\w*?(.*?Warning) (.*)', message)
+        warning_category_groups = re.match(r'^\w*?(.+?Warning) (.*)', message)
         if warning_category_groups is not None:
             warning_category, warning_text = warning_category_groups.groups()
 
-            warning_category_colour = click.style('({})'.format(warning_category), 'cyan')
-            message = '{} {}'.format(click.style(warning_text, fg=None), warning_category_colour)
+            warning_category_colour = _color_text('({})'.format(warning_category), 'cyan')
+            message = '{} {}'.format(_color_text(warning_text, ''), warning_category_colour)
 
     sys.__stdout__.write('{}{}\n'.format(header, message))
     sys.__stdout__.flush()
@@ -97,27 +96,23 @@ class MyFormatter(logging.Formatter):
 
         # Save the original format configured by the user
         # when the logger formatter was instantiated
-        # format_orig = self._fmt
+        format_orig = self._fmt
 
         # Replace the original format with one customized by logging level
-
         if record.levelno == logging.DEBUG:
-            self._style = PercentStyle(MyFormatter.base_fmt)
-
-        elif record.levelno == logging.getLevelName('PRINT'):
-            self._style = PercentStyle(MyFormatter.base_fmt)
+            self._fmt = MyFormatter.base_fmt
 
         elif record.levelno == logging.getLevelName('IMPORTANT'):
-            self._style = PercentStyle(MyFormatter.base_fmt)
+            self._fmt = MyFormatter.base_fmt
 
         elif record.levelno == logging.INFO:
-            self._style = PercentStyle(MyFormatter.base_fmt)
+            self._fmt = MyFormatter.base_fmt
 
         elif record.levelno == logging.ERROR:
-            self._style = PercentStyle(MyFormatter.base_fmt)
+            self._fmt = MyFormatter.base_fmt
 
         elif record.levelno == logging.WARNING:
-            self._style = PercentStyle(MyFormatter.base_fmt)
+            self._fmt = MyFormatter.base_fmt
 
         record.msg = self.ansi_escape.sub('', record.msg)
 
@@ -125,7 +120,7 @@ class MyFormatter(logging.Formatter):
         result = logging.Formatter.format(self, record)
 
         # Restore the original format configured by the user
-        # self._fmt = format_orig
+        self._fmt = format_orig
 
         return result
 
@@ -150,23 +145,33 @@ class LoggerStdout(object):
 
 
 class MyLogger(Logger):
-    """This class is used to set up the logging system."""
+    """This class is used to set up the logging system.
+
+    The main functionality added by this class over the built-in
+    logging.Logger class is the ability to keep track of the origin of the
+    messages, the ability to enable logging of log.warning calls and
+    exceptions, and the addition of colorized output and context managers to
+    easily capture messages to a file or list.
+
+    It is adapted from the astropy logging system.
+
+    """
 
     INFO = 15
-    IMPORTANT = 25
 
     warning_registry = collections.defaultdict(dict)
 
-    def __init__(self, *args, **kwargs):
-
-        self.fh = None
-        self.sh = None
-        self.log_filename = None
-
-        super(MyLogger, self).__init__(*args, **kwargs)
-
     def save_log(self, path):
         shutil.copyfile(self.log_filename, os.path.expanduser(path))
+
+    def _catch_exceptions(self, exctype, value, tb):
+        """Catches all exceptions and logs them."""
+
+        # Now we log it.
+        self.error('Uncaught exception', exc_info=(exctype, value, tb))
+
+        # First, we print to stdout with some colouring.
+        print_exception_formatted(exctype, value, tb)
 
     def warning(self, msg, category=None, use_filters=True):
         """Custom ``logging.warning``.
@@ -182,8 +187,6 @@ class MyLogger(Logger):
         if category is None:
             category = UserWarning
 
-        full_msg = '{0} {1}'.format(category.__name__, msg)
-
         n_issued = 0
         if category in self.warning_registry:
             if msg in self.warning_registry[category]:
@@ -194,7 +197,7 @@ class MyLogger(Logger):
             category_filter = None
             regex_filter = None
             for warnings_filter in warnings.filters:
-                if issubclass(category,  warnings_filter[2]):
+                if issubclass(category, warnings_filter[2]):
                     category_filter = warnings_filter[0]
                     regex_filter = warnings_filter[1]
 
@@ -203,23 +206,14 @@ class MyLogger(Logger):
                     return
 
             if category_filter == 'error':
-                raise ValueError(full_msg)
+                raise ValueError(msg)
 
-        super(MyLogger, self).warning(full_msg)
+        super(MyLogger, self).warning(msg)
 
         if msg in self.warning_registry[category]:
             self.warning_registry[category][msg] += 1
         else:
             self.warning_registry[category][msg] = 1
-
-    def _catch_exceptions(self, exctype, value, tb):
-        """Catches all exceptions and logs them."""
-
-        # Now we log it.
-        self.error('Uncaught exception', exc_info=(exctype, value, tb))
-
-        # First, we print to stdout with some colouring.
-        print_exception_formatted(exctype, value, tb)
 
     def _set_defaults(self, log_level=logging.INFO, redirect_stdout=False):
         """Reset logger to its initial state."""
@@ -246,31 +240,42 @@ class MyLogger(Logger):
         # Catches exceptions
         sys.excepthook = self._catch_exceptions
 
-    def start_file_logger(self, name, log_file_level=logging.DEBUG, log_file_path='~/'):
+    def start_file_logger(self, path, log_file_level=logging.DEBUG):
         """Start file logging."""
 
-        log_file_path = pathlib.Path(log_file_path).expanduser() / '{}.log'.format(name)
-        logdir = log_file_path.parent
+        log_file_path = os.path.expanduser(path)
+        logdir = os.path.dirname(log_file_path)
 
         try:
-            logdir.mkdir(parents=True, exist_ok=True)
 
-            # If the log file exists, backs it up before creating a new file handler
-            if log_file_path.exists():
-                strtime = datetime.datetime.utcnow().strftime('%Y-%m-%d_%H:%M:%S')
-                shutil.move(str(log_file_path), str(log_file_path) + '.' + strtime)
+            if not os.path.exists(logdir):
+                os.mkdir(logdir)
 
-            self.fh = TimedRotatingFileHandler(str(log_file_path), when='midnight', utc=True)
+            if os.path.exists(log_file_path):
+                strtime = datetime.datetime.utcnow().strftime(
+                    '%Y-%m-%d_%H:%M:%S')
+                shutil.move(log_file_path, log_file_path + '.' + strtime)
+
+            self.fh = TimedRotatingFileHandler(
+                str(log_file_path), when='midnight', utc=True)
             self.fh.suffix = '%Y-%m-%d_%H:%M:%S'
         except (IOError, OSError) as ee:
-            log.warning('log file {0!r} could not be opened for writing: '
-                        '{1}'.format(log_file_path, ee), RuntimeWarning)
+            self.warning('log file {0!r} could not be opened for writing: {1}'.format(
+                log_file_path, ee), RuntimeWarning)
         else:
             self.fh.setFormatter(fmt)
             self.addHandler(self.fh)
             self.fh.setLevel(log_file_level)
 
-        self.log_filename = log_file_path
+            self.log_filename = log_file_path
+
+    def set_level(self, level):
+        """Sets levels for both sh and (if initialised) fh."""
+
+        self.sh.setLevel(level)
+
+        if self.fh:
+            self.fh.setLevel(level)
 
 
 logging.setLoggerClass(MyLogger)
